1822502d5004a8438436cbb238ce9323e006940c
Viktorija Taurinskaite <vika.taurinskait@gmail.com>
2024-12-19 21:38:42 +0200

    antras punktas

--- Egzamino_uzduotis.cpp
+++ Egzamino_uzduotis.cpp
@@ -6,7 +6,9 @@ int main()
 { 
     setlocale(LC_ALL, "lt_LT.UTF-8");
     tekstoAnalize("Tekstas.txt");
-    cout << "Suskaiciuoti faile 'Tekstas.txt' esantys zodziai. Rezultatas isvestas i 'zodziu_skaicius.txt' faila" << endl;
+    cout << "Suskaiciuoti faile 'Tekstas.txt' esantys zodziai.\n" <<
+        "Rezultatas isvestas i 'zodziu_skaicius.txt' faila.\n" <<
+        "Sukurta cross-reference lentele - 'cross_reference_lentele.txt' failas" << endl;
 
     system("pause");
     return 0; 
--- Functions.cpp
+++ Functions.cpp
@@ -15,16 +15,18 @@ string pasalintiSkyrybosZenklus(const string& zodis) {
 string sutvarkytiZodzius(const string& zodis) {
     string sutvarkytasZodis = zodis;
 
-    for (char& raide : sutvarkytasZodis) {
-        raide = tolower(static_cast<unsigned char>(raide));
-    }
-
-    sutvarkytasZodis = pasalintiSkyrybosZenklus(sutvarkytasZodis);
-
+    transform(sutvarkytasZodis.begin(), sutvarkytasZodis.end(), sutvarkytasZodis.begin(), ::tolower);
+    
+    sutvarkytasZodis.erase(remove_if(sutvarkytasZodis.begin(), sutvarkytasZodis.end(), ::ispunct), sutvarkytasZodis.end());
+    
     bool raides = any_of(sutvarkytasZodis.begin(), sutvarkytasZodis.end(),
         [](char c) { return isalpha(static_cast<unsigned char>(c)); });
 
-    return raides ? sutvarkytasZodis : "";
+    if (!raides) {
+        return "";
+    }
+
+    return sutvarkytasZodis;
 
 }
 void tekstoAnalize(const string& failoPav) {
@@ -34,12 +36,26 @@ void tekstoAnalize(const string& failoPav) {
     }
 
     map<string, int> zodziu_skaiciavimas;
-    string zodis;
+    map<string, pair<int, set<int>>> zodziuCrossReference;
+    
+    string eilute;
+    int eilutesNumeris = 0;
 
-    while (inputFile >> zodis) {
-        string sutvarkytasZodis = sutvarkytiZodzius(zodis);
-        if (!sutvarkytasZodis.empty()) {
-            zodziu_skaiciavimas[sutvarkytasZodis]++;
+    while (getline(inputFile, eilute)) {
+        eilutesNumeris++;
+
+        stringstream ss(eilute);
+        string zodis;
+
+        while (ss >> zodis) {  
+            string sutvarkytasZodis = sutvarkytiZodzius(zodis);
+
+            if (!sutvarkytasZodis.empty()) {
+                zodziu_skaiciavimas[sutvarkytasZodis]++;
+
+                zodziuCrossReference[sutvarkytasZodis].first = zodziu_skaiciavimas[sutvarkytasZodis];
+                zodziuCrossReference[sutvarkytasZodis].second.insert(eilutesNumeris);
+            }
         }
     }
 
@@ -49,6 +65,12 @@ void tekstoAnalize(const string& failoPav) {
     if (!outputFile.is_open()) {
         cerr << "Nepavyko atidaryti failo." << endl;
     }
+
+    ofstream crossRefFile("cross_reference_lentele.txt");
+    if (!crossRefFile.is_open()) {
+        cerr << "Nepavyko atidaryti failo." << endl;
+    }
+
     outputFile << setw(15) << left << "Zodis"<< setw(15) << left << "Pasikartojimu skaicius" << endl;
     outputFile << string(37, '-') << endl;
     for (map<string, int>::iterator it = zodziu_skaiciavimas.begin(); it != zodziu_skaiciavimas.end(); ++it) {
@@ -58,8 +80,23 @@ void tekstoAnalize(const string& failoPav) {
             outputFile << setw(15) << left << zodis << setw(15) << right << count << endl;
         }
     }
+
+    crossRefFile << setw(15) << left << "Zodis" << setw(15) << left << "Eiluciu numeriai" << endl;
+    crossRefFile << string(37, '-') << endl;
+    for (const auto& it : zodziuCrossReference) {
+        const string& zodis = it.first;
+        int count = it.second.first;
+        const set<int>& eilutes = it.second.second;
+        if (count > 1) {  
+            crossRefFile << setw(15) << left << it.first << setw(15) << left;
+            for (const int line : eilutes) {
+                crossRefFile << setw(2) << left << line << " ";
+            }
+            crossRefFile << endl;
+        }
+    }
     
-    
+    crossRefFile.close();
     outputFile.close();
 }
 
